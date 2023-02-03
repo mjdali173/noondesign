65fc0d416e11f6e8384c3f3d355c61fb558ecfdb
mjdali173 <84237246+mjdali173@users.noreply.github.com>
2023-02-03 23:04:55 +0300

    0

---
+++ assets/js/lang.js
@@ -0,0 +1,91 @@
+
+let ar = document.getElementById("ar");
+let en = document.getElementById("en");
+
+
+let course = document.getElementById("course");
+let collapseOnee = document.getElementById("collapseOnee");
+
+
+let body = document.getElementById("body");
+let collapseTwoo = document.getElementById("collapseTwoo");
+
+let creator = document.getElementById("creator");
+let collapseThreee = document.getElementById("collapseThreee");
+
+let relationship = document.getElementById("relationship");
+let collapseFour = document.getElementById("collapseFour");
+
+
+let Talk = document.getElementById("Talk");
+let collapseFivee = document.getElementById("collapseFivee");
+
+let About = document.getElementById("About");
+let Aboutt = document.getElementById("Aboutt");
+
+
+Ar.onclick = ()=>{
+    setLanugage("Ar");
+    localStorage.setItem("Lang","Ar");
+};
+
+En.onclick = ()=>{
+    setLanugage("En");
+    localStorage.setItem("Lang","En");
+};
+
+onload = ()=>{
+    setLanugage(localStorage.getItem("Lang"));
+	
+};
+function setLanugage(getLanuage){
+if(getLanuage === "Ar"){
+    course.innerHTML = "كورس النقلة _الابداع";
+    collapseOnee.innerHTML = "انها العزيمة لكي تبدا برحلتك لتكتشف حقيقية من انت !*ادوات عملية وتنظيفات عميقة جدا لتغير اشياء في حياتك لم تكن تستطيع ان تغيرها من قبل !*انها صممت لتحطيم المحدودية والاساسات التي كنت تظنها واقعك !*انها تفتح لك الابواب لكي تبدا بالاتصال بوضوح مع ذاتك لتتعرف على المعتقدات المحدودة والالتزامات الخفية المختبئة التي جعلتك عالق من ان تخلق الواقع الذي يعمل !لك وتكتشف شغفك والمبدع الذي انت عليه !*ماذا لو عندما تكون على حقيقتك   هي الهدية التغيير والامكانيات الذي يتطلبه العالم كله ليصبح افضل !";
+	
+    body.innerHTML = "التواصل مع الجسد ولقاء الروح!";
+    collapseTwoo.innerHTML = "هل تعلم أن معظم أجساد الناس ليست متصلة  بكيانهم  ولهذا يشعرون بالضعفماذا لو كان التواصل مع كيانك هو أقوى شيء مررت به حتى لا يستطيع أحد إيقافك؟ * الكٱئن يمكنه أن يكون موجود  في كل مكان حتى يتمكن من  توصيلك بكل شيء في الكون ان اتصال الكآئن مع الجسد هي من تسمح  لكل ما ترغبه يتجلى في عالمك  فالعقل يشتتك دائما ويخلق لك طاقة الحاجة تعيق وصول وتحقيق ما ترغب اليك ....تواصلك مع الكائن يوصلك بكل الاشخاص الذي ترغب بالتواصل معهم الارواح الداعمة الشريك الداعم البزنس الذي ترغب بخلقه الاشياء التي يمكنك ان تغيرها بالكون !!! ح ..ستصبح حينها بوعي وبصحة بجسد اقوى واوعى وادراك يتجاوز عقلك المحدود الكآئن الروح هى التي لديها المعرفة بكل شيئ هنا في هذا ال عن العالم هنا تكون انت فعليا الادراك  القوة والمعجزات والتجلي  وتبدا  حياتك بالتوسع  ! د * ماذا عن الجسد الذي هو مفتاح خلقك! للحياة الذي ترغب * هل تستمع  لجسدك ؟ * كم من ألم تصلب حزن وتوتر الذي تشعر به في جسدك ليس لك بل هو تتطلبه  أمنا الأرض من الناس الذين يطلبون منك المساهمة؟ ألم يحن وقت الإدراك عن هذا الألم؟ ما الذي يرغب جسدك بان يخبرك ؟ * للأجساد قدرات مذهلة على التواصل مع الأجساد الأخرى ، مع البيئة والأرض! * اجسادنا لديها  قدرات جبارة للغاية لكي تاخذ أعباء أجساد أخرى وحبسها داخلها  ، فلنوقف في اي  زمان مكان اختار الجسم شفاء الآخرين دون أخد اذننا دعوننا نخبركم  عن   المحاكاة الحيوية   ب ازالة استئصال التقليد البيولوجي لبصمة الانا والمسارات العالقة  من تقليد مسار الطاقي للاخرين الاجداد ،الاهل والمجتمع  ، الٱمهم ،اجسادهم ، واقعهم المالي ، معاناتهم وخلقهم لحياتهم الذي  نستخدمه في إنشاء حياتنا! * والكثير من التنظيقات من تجارب وجهات نظر  ما اكتسبناه من معتقدات محدودة  من نقطة الخلق لازال هذا الجسد يحفظها عندها يعود جسدك لفطرته  وبقوة خارقة وبتناغم وكانك تطير فرحا تناغم روح وجسد !وهذا ما جئنا نفعله هنا الاستمتاع  ماذا لو تخلينا عن  وجهات النظر المقيدة والمحدودة  التي تعلمناها عن أجسادنا؟ * والكثير لتعرفه في هذه الدورة المذهلة التي من شأنها أن تسمح لك بالحصول على جسم مبدع ومبدع وقوي وواعي للغاية !! * ماذا سيكون الأمر بدأت في التعرف على قدرات  جسدك  وكونك مدركا وواعي  متجاوزا محدودية العقل";
+	
+	
+    creator.innerHTML = "كيف تصبح خلاق الثروة !";
+	collapseThreee.innerHTML = "هل تعاني من تجلي الاموال في حياتك؟ هل تستقبل فقط المال المحدود ؟ماذا لو اصبحت خلاق الثروة وتسمح للاموال الطائلة بان تتجلى في عالمك ؟ ماذا لو ان المال ليس لديه اي  وجهة نظر عليك ويرغب  بالمجيء لاي احد ولكن انت لديك ؟ معظم الناس يقاومون ويرفضون خلق واستقبال الاموال لا واعي منهم لانهم يخافون من ان يستغلهم احد !دعونا نتحرر من كل الوجهات النظر والاحكام والتعريفات  بيننا وبين المال التي تبنينها من الآف السنين والتي جلبناها معنا وجعلناها تشكل واقعنا المالي !؟هل انت على الاستعداد  للتحرر من كل التجارب السابقة التي لازالت لديك مع الاموال !؟دعونا نتحرر من كل العقود الروحية والالتزامات الخفية التي فعلناها ولسنا مدركين لها  مع الاجداد قبل المجيئ لهذه الحياة التي تعيق استقبالنا وخلق الاموال في واقعنا !!الكثير والكثير من التنظيقات ل اللاوعي لدينا الذي لسنا مدركين له الذي يشكل واقعنا ا  والادوات الرائعة مع السحب الطاقي المذهل ستتعرف عليها في هذا الكورس الذي سيغير واقعك المالي وتصبح بعدها انت المال !!!!"
+
+	relationship.innerHTML = "العلاقات الممتعة !";
+    collapseFour.innerHTML = "ماذا لو بدانا خلق علاقة بطريقة مختلفة تعمل معك ؟ هل دائما تجذب نفس العلاقات اليست داعمة؟ الان دعنا نتحرر من تكرار خلق العلاقات التي لا تعمل معنا ؟ فالنخلق علاقة خارجة عن تقليد الخورازمية في المجتمع ،بدون توقعات ، احكام ،استنتاجات ، سيطرة ،تحكم ،مصفوفات ، التي ستسمح لك بخلق اتصال مع الطاقة التي جلبها الكون لنا لنستقبل منها المتعة !اسمح لنفسك بان تستقبل المتعة بالعلاقات ةبعيدا عن ما تعلمته من العادات والتقاليد ، الدين ، المحدودية والمجتمع !!!!"
+ 
+	Talk.innerHTML = "تحدث ،  حرر ، تواصل  ، استقبل من الكينونات !";
+    collapseFivee.innerHTML = "هل تشعر دائما وكان احد يتحكم بواقعك ويعمل عكوسات  وكانه  يوجد احد يعمل ضدك ! دعنا نتحرر من كل الكينونات التي كانت ملتصقة بك وتعمل ضدك لا ترغب بالمساهمة لك ! الكآئنات تبقى عالقة في الامكنة والازمنة هم يعتقدون بانهم هنا الى الابد ولكن نحن لدينا أدوات تعطيهم الوعي لكي يتحرروا ! ماذا لو تحرت من كل العقود الباطنية والعهود و والاجندات السرية بينك وبين اي كآئن من كل حيواة سابقة ابعاد اخرى مما تجعلك حر وتكون انت المسيطر بخلق واقعك ! بعض الأشخاص لا يستطيعون الزواج لان لديهم قسم مع كائن اخر ماذا لو حان وقت التحرر وان تستمتع بخلق علاقات رائعة وممتعة الكٱئنات انواع منهم من هم جيدون ومنهم لا يرغبون بالمساهمة ، فالنتعرف على كآئنات النور (الملائكة )والتي ترغب بشدة للمساهة فالنتعلم طريقة تواصلنا معهم ونستقبل مساهماتهم !"
+
+   About.innerHTML = "لمحة عن ميرفت !";
+    Aboutt.innerHTML = "، اذا كان هدفك  في الحياة المرح المتعة والإبداع  </br>ماذا سوف تغير ؟</br>ميرفت صيداني صاحبة شركة </br>your shift _ creative </br>ميسرة التغيير </br>ميرفت تيسر تغيير حياة النااس لكي يتعرفوا على قدراتهم التي سوف تغير واقعهم ليختاروا الاعظم  لهم !</br>تؤمن ميرفت بان كل واحد منا لديه قدرات هائلة  وابداعه وشغفه في الحياة .</br>تعرف ميرفت بقوتها وطاقتها الرائعة والسلسة التي لديها بتغيير حياة الناس مع ادواتها الرائعة والسهلة والبسيطة جدا  لكي تساعد الناس بان يتخلصوا من المحدودية ، والبرامج التي تبقيهم من ان يكونوا احرار  لكي يعيشوا حياتهم ، يخلقوا الحياة التي يرغبونها ويجدوا شغفهم وابداعهم في الحياة .</br>انها تساعدهم بان يبقوا دائما حاضرين واعيين لكي يختاروا الاعظم </br>عندما نكون نحن على حقيقتنا ستكون الجنة هنا على الارض </br>فالنخلق حياة مختلفة  حياتنا التي نرغب  فلتكون لنا حياة افضل وعالم افضل !";
+    
+}
+else if(getLanuage ==="En"){
+    course.innerHTML = "The shift_creative course !";
+   collapseOnee.innerHTML = "*the shift _creative course is an invitation to start in your journey to discover who you truly are !*pragmatic tools and deep clearings to change things in your life that you haven't  been able to change !*Its designed to break all the limitation and the foundation of what you think your reality is, so you can create the reality you desire ! It opens the doors for you to start communicating clearly with yourself to acknowledge the hidden limiting beliefs and commitments that keeps you from creating the reality that works for you. It also helps discovering your passion and the creative you !!so what if you truly being you is the gift the change and the possibility the world requires ?";
+   
+   
+    body.innerHTML = "connect with the body and meet the being !";
+    collapseTwoo.innerHTML = "Did you know that most of peoples' bodies are not connected to their being that's why they feel weak !*what if connecting with your being is the most powerful thing you have ever had so nobody can stop you ?*The being can be everywhere and can connect you with everything in the universe, allow everything you desire to show up in your life, mind always confuses you and create need energy that will obstruct achieving your desires !*you will become more aware more conscious more healthy with a powerful body beyond limitation ! *The being that has the knowingness, awareness of everything, with connecting to it you are being the potency, awareness, magic, miracle you truly be here and your life will expand more!!! *what about the body that is the key of your creation?*Do you listen to your body ?*How much pain stiffness sadness and stress do you have in your body that is not yours but it is a demand from the mother earth its the people who are asking contribution from you? isn't the time to release this pain ?*Bodies have amazing capacities to communicate with other bodies, with the environment and the earth !*bodies have very powerful capacities so they can take other bodies burdens and locked them in theirs let's stop everywhere the body chose that to heal others without taking our permission ! *let us despite and eliminate all the biomimetic mimicry our ansesctors, parents, society, our friends pathways, their bodies, their relationship, their pain , financial reality their limitation thier creation of their life are we using ! *what if we let go all the  limiting points of view we have learned about our bodies ?* and a lot to know in this amazing course that would allow you to have a creative generative , powerful and very conscious body !!*what would it be like if you start acknowledging your body and your being's capacities, being aware and beyond your limited mind ?!";
+	
+	
+    creator.innerHTML = "Become the wealth creator!";
+    collapseThreee.innerHTML = "are you suffering from money in your life? Are you recieving enough money ?what if you become the wealth creator and allow huge amounts of money to show up in your life ? what if money doesn't have any points of view about you and desires to come to anyone but you have points of view that doesn't allow it to show up for you? Most of people resist and refuse creating recieving money unconsciously because they are afraid from others to abuse them !! let us free from all of the points of view, judgement,  definitions between us and money that we brought from other lifetimes, this lifetime to allow ourselves to create generate money with total ease !Are you ready to release from all lifetimes experiences that you still hold with money ? let us free ourselves from all commitment, contracts, that we have done with our ancestors that we are not aware of before coming here that obstruct us from creating beyond them !Many and many of clearings to our unconscious mind we are not aware of and we will clear ! In addition there are magical  tools magical pulling energy  you are going to acknowledge through this course that would allow you to create generate the wealth in your life and you become the money !";
+  
+  relationship.innerHTML = "joyful relationship !";
+    collapseFour.innerHTML = "What if you create your relationship in a different way that works for you ?Are you always attracting the same not relevant relationships ?Here we go let us free from creating the relationship that doesn't work for us !!let's create a relationship without any expectations, judgement, conclusion, out of control, definitions, concentricities that would allow us to create a communion with the energy the universe sends us to  receive from it  !!!! allow yourself to receive the joy from your relationships far away from what have you learned from cultures, religions, limitations , and society !"
+   
+   Talk.innerHTML = "Talk , clear , receive , and communicate with entities !";
+    collapseFivee.innerHTML = "Do you feel that somebody controls you? somebody control your reality? and working against you that keeps you from living the life you love ?let us free from these entities that are being with you all the time and wouldn't like to contribute to you ! entities are stuck in time and place they think this is their place forever, they are just waiting someone like you who has concsiousness to set them free, we have tools to set them free !So what if you free yourself from all commitment, agreements, contracts, that you have done with any entities, our dead relatives that keeps you from having freedom to live your life and set you free ! some people couldn't have a relationship because they have ows, vows , with another entities from other lifetimes what if this is the time to set yourself free and have a partner ? entities are types some are good others wouldn't love to communicate what if we started receiving contribution from light entities that we call them angels that really would love to contribute to us!"
+
+	About.innerHTML = "About Mirvat";
+    Aboutt.innerHTML = "-if your purpose in life was to have joy, fun, passion and creativity what would you change?</br>*Mirvat sidani , the founder of your shift _creative  company .</br>-Facilitator of change Mirvat is facilitating the change for people's realities to acknowledge their capacities that will transform their lives to choose greater !</br>-Mirvat believes that everyone has their own capacities and their passion to create here to have more fun ! </br>-she is known as a powerful person with wonderful energy and the capacities she has to transform people's lives , with her simple tools to assist people to remove limitations, programs, limiting beliefs that holding are them back from the freedom to live their lives and create the life they truly desire !!! </br>-she assists people to be  aware, present, conscious and to choose greater and create a better life, find their passion and creativity ! </br>-How can we contribute to each other and to the whole planet to become better !!!</br>-when we are truly being us our lives will be heaven on earth !</br>-let us create a different reality to have a better life and a better world !";
+    
+}
+
+
+}
+
+
+
+
